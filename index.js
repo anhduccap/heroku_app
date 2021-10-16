87f7daa72efc94a4e8507c9c2b7214d6d756f9d4
Anh Duc Cap <anhduccap@gmail.com>
2021-10-16 10:48:14 +0700

    update api

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Success')
-})
+    res.status(200).send({
+        status: '200',
+        msg: 'Success!',
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`);
